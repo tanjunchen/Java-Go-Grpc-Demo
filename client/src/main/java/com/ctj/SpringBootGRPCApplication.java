4d5e3c0acbe9b564f393a83fda72c2c1795ce08d
tanjunchen <2799194073@qq.com>
2020-09-17 20:51:22 +0800

    feat:client-go list and watch example

--- client/src/main/java/com/ctj/SpringBootGRPCApplication.java
+++ client/src/main/java/com/ctj/SpringBootGRPCApplication.java
@@ -1,7 +1,10 @@
 package com.ctj;
 
+import com.tanjunchen.demo.ServiceServiceGrpc;
+import com.tanjunchen.demo.SyncServiceResponse;
 import io.grpc.netty.shaded.io.grpc.netty.NegotiationType;
 import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
+import io.grpc.stub.StreamObserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,7 +15,47 @@ public class SpringBootGRPCApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootGRPCApplication.class, args);
+        //test1();
+        test2();
+    }
 
+    private static void test2() {
+        io.grpc.Channel channel = NettyChannelBuilder.forAddress(host, port)
+                .negotiationType(NegotiationType.PLAINTEXT)
+                .build();
+
+        ServiceServiceGrpc.ServiceServiceStub serviceServiceStub = ServiceServiceGrpc.newStub(channel);
+
+        // 异步存根
+        StreamObserver<com.tanjunchen.demo.SyncServiceResponse> responseObserver = new StreamObserver<SyncServiceResponse>() {
+            @Override
+            public void onNext(SyncServiceResponse value) {
+                System.out.println("返回了结果 ==> " + value);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+
+            }
+        };
+
+        com.tanjunchen.demo.SyncServiceRequest request = com.tanjunchen.demo.SyncServiceRequest.newBuilder().build();
+        StreamObserver<com.tanjunchen.demo.SyncServiceRequest> result = serviceServiceStub.syncServiceWatchListService(responseObserver);
+        result.onNext(request);
+        result.onCompleted();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void test1() {
         io.grpc.Channel channel = NettyChannelBuilder.forAddress(host, port)
                 .negotiationType(NegotiationType.PLAINTEXT)
                 .build();
@@ -30,7 +73,6 @@ public class SpringBootGRPCApplication {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
 
